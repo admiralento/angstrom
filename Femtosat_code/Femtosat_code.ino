f0263aa1801d5522e63aa1c1162387e72dd9f339
Ammon Wolfert <admiralento@gmail.com>
2019-10-19 14:25:06 -0600

    Added Radio

--- Femtosat_code/Femtosat_code.ino
+++ Femtosat_code/Femtosat_code.ino
@@ -1,14 +1,14 @@
 /*
  *    Project Angstrom
  *    Femtosat Flight Software
- * 
+ *
  *    BYU
  *    Autumn 2019
- * 
+ *
  *    Austin Stone, David Chang, and Ammon Wolfert
- *    
+ *
  *    To install required libraries on your computer
- *    please move the folders from your Github folder 
+ *    please move the folders from your Github folder
  *    under "Arduino Libraries"
  *    into Documents --> Arduino --> libraries
  *    There should be a RFM69 and MPU9250 Library in there
@@ -20,38 +20,59 @@
 #include <MPU9250.h>
 #include <SparkFunBME280.h>
 
+//Set Defintions
+#define NETWORKID     0   // Must be the same for all nodes
+#define MYNODEID      1   // My node ID
+#define TONODEID      2   // Destination node ID
+#define FREQUENCY     RF69_915MHZ
+
+RFM69 radio;
 
 // Create constants here
-const int misoPin = 7;
-const int mosiPin = 6;
-const int sckPin = 5;
-const int nssPin = 4;
-const int intPin = 3;
-const int sclPin = 13;
-const int sdaPin = 12;
+#define MISOPIN      7
+#define MOSIPIN      6
+#define SCKPIN       5
+#define NSSPIN       4
+#define INTPIN       3
+#define SCLPIN       13
+#define SDAPIN       12
+
+//Variables
+char sendBuffer[62];
+int bufferIndex = 0;
 
 void setup() {
 
-  //set the pinmode of all communication pins
-  //begin serial communication (for debugbing with computer)
-  RFM69 radio;
-  
+  //Init radio communication
+  radio.initialize(FREQUENCY, MYNODEID, NETWORKID);
+  radio.setHighPower();
+  radio.setPowerLevel(20);
+  radio.encrypt(0);
+
   Serial.begin(9600);  //Initialize Serial Communication
-  
+
 }
 
 void loop() {
 
   //Fetch Data from IMU
   //Save to buffer
+  sendBuffer[bufferIndex] = IMU.read();
+  bufferIndex++;
 
   //Fetch Data from Atmospheric Sensor
   //Save to buffer
+  sendBuffer[bufferIndex] = BME.read();
+  bufferIndex++;
 
   //If enough data saved in buffer
   //Send data packet by radio
   //and write to memory module
+  if (bufferIndex == 61) {
+    radio.send(TONODEID, sendbuffer, bufferIndex);
+    bufferIndex = 0;
+  }
 
   //Repeat
- 
+
 }
